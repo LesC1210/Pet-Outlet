da51c2bc0759335456134620bbe066c34811c270
Williams Kelleytina <kelleytina.williams@infotech.hamptonu.edu>
2017-07-12 11:34:36 -0400

    my second
    Signed-off-by: Williams Kelleytina <kelleytina.williams@infotech.hamptonu.edu>

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+import os 
+import urllib2
+import urllib
+
+from google.appengine.ext import ndb
+
+class User(ndb.Model):
+	userEmail = ndb.StringProperty()
+	userPass= ndb.StringProperty()
+
+jinja_environment = jinja2.Environment(
+	loader=jinja2.FileSystemLoader(
+		os.path.dirname(__file__)))
+
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+	def get(self):
+		template = jinja_environment.get_template('pet-outlet.html')
+		self.response.write(template.render())
+	
+class WelcomeHandler(webapp2.RequestHandler):
+	def get(self):
+		template = jinja_environment.get_template('welcome.html')
+		self.response.write(template.render())
+	
+class SignUpHandler(webapp2.RequestHandler):
+	def get(self):
+		template = jinja_environment.get_template('sign_up.html')
+		self.response.write(template.render())
+		 
+	def post(self):
+		username= self.request.get('name')
+		petname= self.request.get('pet_name')
+
+		template= jinja_environment.get_template('welcome.html')
+		self.response.write(template.render(
+			{
+				'name': username,
+				'pet_name': petname,
+			}))
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+	('/', MainHandler),
+	('/welcome',WelcomeHandler),
+	('/signup', SignUpHandler),
+
 ], debug=True)
+
+class WelcomeHandler(webapp2.RequestHandler):
+	def get(self):
+		template = jinja_environment.get_template('welcome.html')
+		self.response.write(template.render())
